c829e01bc20d25dd4299d431b0a98e4a9bdec7a6
Spencer White <swhite215@live.com>
2017-03-06 10:44:19 -0500

    Added createLights function

--- js/main.js
+++ js/main.js
@@ -99,3 +99,39 @@ function createScene() {
     camera.updateProjectionMatric();
   }
 }
+
+//declare createLights variables
+var hemisphereLight, shadowLight;
+
+function createLights() {
+  //A hemisphere light is a gradient colored light.
+  //THREE.HemisphereLight(sky color, ground color. intensity)
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9);
+
+  //A directional light shines from a specific direction
+  //It acts like the sun, that means all the rays produced are parallel
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+
+  //set the direction of the light
+  shadowLight.position.set(150, 350, 350);
+
+  //Allow shadow casting
+  shadowLight.castShadow = true;
+
+  //define the visible area of the projected shadow
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+
+  //define the resolution of the shadow; the higher the better
+  //but also the more expensive and less performant
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  //to activate the lights, add them to the scene
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
+}
