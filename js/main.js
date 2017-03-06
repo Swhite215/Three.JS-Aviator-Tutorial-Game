84e682e11a3abbc016bf09f571c2b671849aeb3b
Spencer White <swhite215@live.com>
2017-03-06 11:17:36 -0500

    Created Cloud Object

--- js/main.js
+++ js/main.js
@@ -173,3 +173,44 @@ function createSea() {
   //add the mesh of the sea to the scene.
   scene.add(sea.mesh);
 }
+
+Cloud = function() {
+  //Create an empty container that will hold the different parts of the Cloud
+  this.mesh = new THREE.Object3D();
+
+  //create a cube geometry that will be duplicated.
+  //THREE.BoxGeometry(width, height, depth);
+  var geom = new THREE.BoxGeometry(20,20,20);
+
+  //create a material, a simple white material
+  var mat = new THREE.MeshPhongMaterial({
+    color: Colors.white
+  });
+
+  //duplicate the geometry a random number of times
+  var nBlocs = 3+Math.floor(Math.random()*3);
+  
+  for (var i = 0; i < nBlocs; i++) {
+
+    //create the mesh by cloning the geometry
+    var m = new THREE.Mesh(geom, mat);
+
+    //set the position and the rotation of each cube randomly
+    m.position.x = i*15;
+    m.position.y = Math.random()*10;
+    m.position.z = Math.random()*10;
+    m.rotation.z = Math.random()*Math.PI*2;
+    m.rotation.y = Math.random()*Math.PI*2;
+
+    //set the size of the cube randomly
+    var s = .1 + Math.random()*.9;
+    m.scale.set(s,s,s);
+
+    //allow each cube to casse and to receive shadows
+    m.castShadow = true;
+    m.receiveShadow = true;
+
+    //add the cube to the container we first created
+    this.mesh.add(m);
+  }
+}
